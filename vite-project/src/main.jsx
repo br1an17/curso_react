0e077547f0aac493fdf9a183541873ba15e0ba6b
br1an17 <brianzairpaez@gmail.com>
2025-09-16 10:43:46 -0300

    agregamos un form

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { Boton } from './boton.jsx'
+import { Boton } from './componentes/boton.jsx'
+import { Form } from './componentes/form.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Boton texto="Click me" color="lightblue" />
+    <Form />
   </StrictMode>,
 )
