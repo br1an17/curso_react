9f62befb7d7cd07884737ca93553d4847f8ba9c6
br1an17 <brianzairpaez@gmail.com>
2025-10-23 18:16:43 -0300

    agregamos mas articulos y acomodamos el boton

--- vite-project/src/componentes/item/item.jsx
+++ vite-project/src/componentes/item/item.jsx
@@ -8,7 +8,7 @@ export const Item = ({ name,price,description,imageUrl,children }) => {
     <img src={imageUrl} alt={description} />
     <div className='product-info'>
     <h2 className="product-title">{name}</h2>
-    <p className="product-price">Precio:${price}</p>
+    <p className="product-price">Precio: $ {price}</p>
     <p className="product-description">{description}</p>
     </div>
     {children}
