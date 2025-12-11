b961447edc4e8c41a1f24c62cb5bf28ae91d4560
br1an17 <brianzairpaez@gmail.com>
2025-12-11 15:56:34 -0300

    arreglo de mayuscula 2

--- tiendITa/src/componentes/itemListContainer/itemListContainer.jsx
+++ tiendITa/src/componentes/itemListContainer/itemListContainer.jsx
@@ -30,7 +30,7 @@
 
 
 import { useEffect, useState } from "react"
-import { ItemList } from "../ItemList/ItemList"
+import { ItemList } from "../ItemList/itemList"
 import { useParams } from "react-router-dom"
 import './ItemListContainer.css'
 import { getProducts } from "../../services/products"
